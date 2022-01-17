ed023afe2458fb841f8a7b042ab728c01bcebcb9
ascend2001 <avnishsengupta1@g.ucla.edu>
2022-01-16 20:01:29 -0800

    Combined the components part 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import AuthorDisplay from './Components/creatorPage/AuthorDisplay';
+import CreatedWorksCard from './Components/creatorPage/creatorAvnish';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthorDisplay authId="recbWFQEU8NMQXASa" />
+      <CreatedWorksCard authorId="recbWFQEU8NMQXASa" />
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit
+//           <code>src/App.js</code>
+//           and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
