eb65646017aa03f4b667354a2bb4d92988e3765c
Caroline DebBaruah <carolinedebbaruah@MacBook-Pro-2.local>
2022-01-18 20:39:43 -0800

    author image and links

--- src/Components/creatorPage/AuthorInfo.js
+++ src/Components/creatorPage/AuthorInfo.js
@@ -1,23 +1,29 @@
-/* eslint-disable react/forbid-prop-types */
 import PropTypes from 'prop-types';
 import React from 'react';
 import './Author.css';
 
-export default function AuthorInfo({ author, bio, link }) {
+export default function AuthorInfo({
+  author, bio, links, authorPic,
+}) {
+  const linksArray = links.split('\n');
+  linksArray.pop();
   return (
     <>
       <div className="Header">
         {author}
+        <img width="10%" height="10%" align="center" src={authorPic} alt="author" />
       </div>
       <div className="Paragraph">
         {bio}
       </div>
       <br />
       <div className="Extra">
-        Here are some links!:
+        Click here to learn more!:
       </div>
       <div>
-        {link}
+        {linksArray.map((element) => (
+          <p><a href={element}>{element}</a></p>
+        ))}
       </div>
     </>
   );
@@ -26,5 +32,6 @@ export default function AuthorInfo({ author, bio, link }) {
 AuthorInfo.propTypes = {
   author: PropTypes.string.isRequired,
   bio: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
+  links: PropTypes.string.isRequired,
+  authorPic: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
 };
