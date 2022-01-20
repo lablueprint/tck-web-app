60ce6bb906520a81999a95c5eb37d74678b6260d
Ryan Yeo <ryandc.yeo@gmail.com>
2022-01-19 23:05:36 -0800

    First internal tck demo day

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <div id="book-card">
           <CardsDisplay />
-          <CardsDisplay />
         </div>
       </header>
     </div>
--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,33 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Card.css';
+
+// airtable configuration
+const Airtable = require('airtable');
+
+const airtableConfig = {
+  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
+  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+};
+
+const base = new Airtable({ apiKey: airtableConfig.apiKey })
+  .base(airtableConfig.baseKey);
 
 export default function Card({ title, author, image }) {
-  return (
-    <div>
+  const [authorVar, setAuthor] = useState();
+
+  const authorRecords = () => {
+    base('Creator').find(author, (err, record) => {
+      if (err) { console.error(err); }
+      setAuthor(record);
+      console.log(record);
+    });
+  };
+
+  useEffect(authorRecords, []);
+
+  return ( // App.css is flexed as a column
+    <div id="card">
       <div id="title">
         Title:
         {' '}
@@ -12,11 +36,11 @@ export default function Card({ title, author, image }) {
       <div id="author">
         Author:
         {' '}
-        {author}
+        { authorVar !== undefined ? (authorVar.fields.name) : 'loading'}
       </div>
       <div id="image">
         Image:
-        {' '}
+        <img id="book-cover" src={image} alt="book_cover" />
         {image}
       </div>
     </div>
