5322e38efb22161bd556b9b7a6385497cfbd35df
Solia Valentine <soliavalentine@gmail.com>
2022-01-23 19:38:54 -0800

    Sprint 1 Collection Header

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import Collection from './Components/Collections/Collection';
+import CollectionDisplay from './Components/Collections/CollectionDisplay';
 
 function App() {
   return (
     <div>
-      <Collection Collid="recbiuRn5FMzmcSil" />
+      <CollectionDisplay />
     </div>
   );
 }
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Collection />
-      </header>
-    </div>
-  );
-} */
 
 export default App;
--- src/Components/Collections/CollectionDisplay.js
+++ src/Components/Collections/CollectionDisplay.js
@@ -1,38 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Collection from './Collection';
 
-function CollectionDisplay({ collection }) {
-  // const [collections.getIDs, setCollection] = useState([]);
-  // setCollection(collection);
-  // useEffect(getCollection, [collectionNum]);
-  /* renderItems = () => {
+const Airtable = require('airtable');
 
-    return (
-      <AllCollections>
-        {this.state.news.map((card) => (
-          <Card
-            key={card.id} // Make sure you use a unique key identifier for React
-            image={card.imageUrl} // This is the url of the image for the current object
-            inside this.state.news.YOUR_CURRENT_OBJECT
-            header={card.title}
-            meta={card.type}
-            description={card.description}
-          />
-        )}
-      </AllCollections>
-    ) */
-  return collection.map((id) => (
-    <Collection
-      id={id}
-    />
+const airtableConfig = {
+  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
+  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+};
+const base = new Airtable({ apiKey: airtableConfig.apiKey })
+  .base(airtableConfig.baseKey);
+
+function CollectionDisplay() {
+  const [collections, setCollections] = useState([]);
+  const getCollections = () => {
+    base('Collection').select({ view: 'Grid view' }).all() // Gets + returns all records
+      .then((records) => { // Takes in returned records + calls setPosts to store in posts arr
+        setCollections(records);
+      });
+  };
+
+  useEffect(getCollections, []);
+
+  return collections.map((collection) => (
+    <div>
+      <Collection
+        Collid={collection.id}
+      />
+    </div>
   ));
 }
-// pass in a prop of ids
-// using id, save record in a state
-// once we are done saving all records,pass the array of records? to the map function
+
 CollectionDisplay.propTypes = {
-  numPosts: PropTypes.number.isRequired,
+  numcollections: PropTypes.number.isRequired,
 };
 
 export default CollectionDisplay;
