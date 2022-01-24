4f7147dbcd01b715fec708ceadebf36120c26eba
Dan Lee <danlee01@ucla.edu>
2022-01-24 15:02:57 -0800

    moved BookPage to Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+/*
+import {
+  BrowserRouter as Router, Route, Routes, Link,
+} from 'react-router-dom';
+*/
 import './App.css';
 
-import BookPage from './Components/BookPage/BookPage';
+import BookPage from './Pages/BookPage/BookPage';
 
 function App() {
+  return (
+    <BookPage bookId="rectqkZI0hdvX5CMP" />
+  );
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +22,7 @@ function App() {
       </header>
     </div>
   );
+  */
 }
 
 export default App;
