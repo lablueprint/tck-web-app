ccca43093cd0397299ef0705843242cfb9323448
Dan Lee <danlee01@ucla.edu>
2022-01-30 19:42:05 -0800

    Refactored error checking

--- src/Pages/BookPage.js
+++ src/Pages/BookPage.js
@@ -4,11 +4,9 @@ import {
 } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import BookSynopsis from '../Components/BookSynopsis';
+import Logo from '../Assets/Images/TCK PNG Logo.png';
 
 const Airtable = require('airtable');
-
-/* This is the default image if TCK forgets to populate the image field. */
-const TCK_LOGO = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/119db2be-192f-4a50-94ff-2c4f021384e3/TCK_PNG_Logo.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220124%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220124T032111Z&X-Amz-Expires=86400&X-Amz-Signature=bf863cbd890a1ac3d8ef071ad1b02652642ae24de0e5f10c6fcc4bfcef62a97f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22TCK%2520PNG%2520Logo.png%22&x-id=GetObject';
 
 const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY })
   .base(process.env.REACT_APP_AIRTABLE_BASE_KEY);
@@ -46,89 +44,109 @@ function BookPage() {
     getEntries();
   }, [bookId]); // Runs on mount and on change of bookId
 
-  if (book && author && illustrator) {
-    const title = (book.get('title')) ? book.get('title') : 'Untitled Book';
-    const authorName = (author.get('name')) ? author.get('name') : 'Unknown Author';
-    const illustratorName = (illustrator.get('name')) ? illustrator.get('name') : 'Unknown Illustrator';
-    const desc = (book.get('description')) ? book.get('description') : 'It\'s a book. with words. **gasp**';
-    const image = (book.get('image')) ? book.get('image') : [{ url: TCK_LOGO }];
-    const imageURL = image[0].url;
-    const readAloudURL = (book.get('read_aloud_link')) ? book.get('read_aloud_link') : null;
-    const bookshopURL = (book.get('bookshop_link')) ? book.get('bookshop_link') : null;
-    const educatorURLs = (book.get('educator_guide_links')) ? book.get('educator_guide_links') : [];
+  if (!book) {
+    return <div>Scouring our library...</div>;
+  }
 
-    const synopsisProps = {
-      title, authorName, illustratorName, desc, imageURL,
-    };
-    const synopsis = <BookSynopsis {...synopsisProps} />;
+  let title = 'Untitled Book';
+  let authorName = 'Unknown Author';
+  let illustratorName = 'Unknown Illustrator';
+  let desc = 'It\'s a book. with words. **gasp**';
+  let image;
+  let readAloudURL;
+  let bookshopURL;
+  let educatorURLs;
 
-    /* READ_ALOUD EMBED */
-    let readAloud = (<div />);
-    if (readAloudURL) {
-      /*
+  if (book) {
+    title = (book.get('title')) ? book.get('title') : title;
+    desc = (book.get('description')) ? book.get('description') : desc;
+    image = (book.get('image')) ? book.get('image') : [{ url: Logo }];
+    readAloudURL = (book.get('read_aloud_link')) ? book.get('read_aloud_link') : null;
+    bookshopURL = (book.get('bookshop_link')) ? book.get('bookshop_link') : null;
+    educatorURLs = (book.get('educator_guide_links')) ? book.get('educator_guide_links') : [];
+  }
+
+  if (author) {
+    authorName = (author.get('name')) ? author.get('name') : authorName;
+  }
+
+  if (illustrator) {
+    illustratorName = (illustrator.get('name')) ? illustrator.get('name') : illustratorName;
+  }
+
+  const imageURL = image[0].url;
+
+  const synopsisProps = {
+    title, authorName, illustratorName, desc, imageURL,
+  };
+
+  /* READ_ALOUD EMBED */
+  let readAloud = (<div />);
+  if (readAloudURL) {
+    /*
         read_aloud_link is defined as a 'watch' URL,
         so we must extract the embed code from this URL
           WARNING: This code will break if the read_aloud_link
           is not in the form of (anything)('watch?v=')(video id)$
       */
-      const videoId = readAloudURL.split('watch?v=')[1];
-      readAloud = (
-        <div className="video-responsive">
-          <iframe
-            width="853"
-            height="480"
-            src={`https://www.youtube.com/embed/${videoId}`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
-          />
-        </div>
-      );
-    }
-
-    /* BOOKSHOP LINK, LinkUI from MaterialUI to not be confused with react-router */
-    let bookshop = <div />;
-    if (bookshopURL) {
-      bookshop = (
-        <LinkUI href={bookshopURL} rel="noreferrer" target="_blank">
-          Buy
-          {' '}
-          {title}
-          !
-        </LinkUI>
-      );
-    }
-
-    /* EDUCATOR GUIDE LINK */
-    let educators = <div />;
-    if (educatorURLs) {
-      educators = (
-        <List>
-          {educatorURLs.map((url) => (
-            <ListItem button component="a" href={url} key={url} alignItems="center">
-              <ListItemText sx={{ textAlign: 'center' }} primary={url} />
-            </ListItem>
-          ))}
-        </List>
-      );
-    }
-
-    return (
-      <Paper variant="outlined">
-        {synopsis}
-        {readAloud}
-        {bookshop}
-        <h4>Educator Guides</h4>
-        {educators}
-      </Paper>
+    const videoId = readAloudURL.split('watch?v=')[1];
+    readAloud = (
+      <div className="video-responsive">
+        <iframe
+          width="853"
+          height="480"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
+      </div>
     );
   }
-  return <div>Scouring our library...</div>;
+
+  /* BOOKSHOP LINK, LinkUI from MaterialUI to not be confused with react-router */
+  let bookshop = <div />;
+  if (bookshopURL) {
+    bookshop = (
+      <LinkUI href={bookshopURL} rel="noreferrer" target="_blank">
+        Buy
+        {' '}
+        {title}
+        !
+      </LinkUI>
+    );
+  }
+
+  /* EDUCATOR GUIDE LINK */
+  let educators = <div />;
+  if (educatorURLs) {
+    educators = (
+      <List>
+        {educatorURLs.map((url) => (
+          <ListItem button component="a" href={url} key={url} alignItems="center">
+            <ListItemText sx={{ textAlign: 'center' }} primary={url} />
+          </ListItem>
+        ))}
+      </List>
+    );
+  }
+
+  return (
+    <Paper variant="outlined">
+      <BookSynopsis {...synopsisProps} />
+      {readAloud}
+      {bookshop}
+      <h4>Educator Guides</h4>
+      {educators}
+    </Paper>
+  );
 }
 
 export default BookPage;
 
 /* Notes for future development
   1. For tags, use MaterialUI chips
+  2. Q: For when Book or Creator is missing, do we want to display defaults or raise error
+      saying that we couldn't pull the correct information for this entry?
 */
