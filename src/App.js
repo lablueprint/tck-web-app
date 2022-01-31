4322a69b966bee44944622bc9035412b34603d74
ascend2001 <avnishsengupta1@g.ucla.edu>
2022-01-30 19:32:00 -0800

    Resolved pull request comments

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import React from 'react';
-// import logo from './logo.svg';
 import {
-  BrowserRouter as Router, Route, Routes, Link,
+  BrowserRouter as Route, Routes,
 } from 'react-router-dom';
 import './App.css';
 import CreatorPage from './Pages/CreatorPage';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Link to="/creator/recbWFQEU8NMQXASa">Creator Page</Link>
-        {/* <CreatorPage authId="recbWFQEU8NMQXASa" /> */}
-        <Routes>
-          <Route exact path="/creator/:id" element={<CreatorPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Routes>
+        <Route exact path="/creator/:id" element={<CreatorPage />} />
+      </Routes>
+    </div>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit
-//           <code>src/App.js</code>
-//           and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
