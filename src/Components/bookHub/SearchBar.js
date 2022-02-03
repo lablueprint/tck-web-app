43e0661f4062268b7a7c852ee97aaa19479556d6
Dan Lee <danlee01@ucla.edu>
2022-02-02 23:00:41 -0800

    first commit'

--- src/Components/bookHub/SearchBar.js
+++ src/Components/bookHub/SearchBar.js
@@ -2,7 +2,29 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 
 export default function SearchBar() {
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleChange = async (input) => {
+    const filtered = 
+  }
+
   return (
-    <TextField id="standard-basic" label="Standard" variant="standard" />
+    <div>
+      <TextField
+        id="standard-basic"
+        label="Standard"
+        placeholder="Enter keyword"
+        variant="standard"
+        onChange={handleChange}
+      >
+
+      </TextField>
+
+      searchTerms.map(() => (
+        
+      ))
+    </div>
   );
 }
