3caabcef1cd8a53e8b139616dbea86e1644178fa
Dan Lee <danlee01@ucla.edu>
2022-02-09 20:23:59 -0800

    scaffolding

--- src/Components/bookHub/SearchBar.js
+++ src/Components/bookHub/SearchBar.js
@@ -1,30 +1,53 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
+import React, { } from 'react';
+import { PropTypes } from 'prop-types';
+import {
+  TextField, Select, FormControl, InputLabel, MenuItem,
+} from '@mui/material';
 
-export default function SearchBar() {
-  const handleChange = (event) => {
-    setName(event.target.value);
+function SearchBar({ setSearchTerms, setDefaultSearch }) {
+  // const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      // console.log(e.target.value);
+      setSearchTerms(e.target.value);
+      // setValue('');
+    } else {
+      console.log(e.target.value);
+      // setValue(value + e.target.value);
+    }
   };
-
-  const handleChange = async (input) => {
-    const filtered = 
-  }
 
   return (
     <div>
+      <FormControl autoWidth>
+        <InputLabel id="search-by-label">search by</InputLabel>
+        <Select
+          labelId="search-by-label"
+          id="search-by"
+          defaultValue
+          label="search by"
+          onChange={(e) => setDefaultSearch(e.target.value)}
+        >
+          <MenuItem value>Title, Description, Identity</MenuItem>
+          <MenuItem value={false}>Author, Illustrator</MenuItem>
+        </Select>
+      </FormControl>
       <TextField
-        id="standard-basic"
-        label="Standard"
+        id="filled-basic"
+        label="search"
         placeholder="Enter keyword"
         variant="standard"
-        onChange={handleChange}
-      >
-
-      </TextField>
-
-      searchTerms.map(() => (
-        
-      ))
+        onKeyPress={handleChange}
+      />
     </div>
   );
 }
+
+export default SearchBar;
+
+SearchBar.propTypes = {
+  setSearchTerms: PropTypes.func.isRequired,
+  setDefaultSearch: PropTypes.func.isRequired,
+};
