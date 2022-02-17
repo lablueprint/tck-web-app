5b34d751bcef3f2a15c0b3604ee3fcac5340a7cb
Caroline DebBaruah <carolinedebbaruah@MacBook-Pro-2.local>
2022-02-16 22:09:03 -0800

    CollectionInfo Display

--- src/Pages/Dictionary.js
+++ src/Pages/Dictionary.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import WordTermDisplay from '../Components/Dictionary/WordTermDisplay';
-import './PagesTemp.css';
+import React, { useState, useEffect } from 'react';
+import WordTerm from '../Components/Dictionary/WordTerm';
 
-function Dictionary() {
-  return (
+// airtable configuration
+const Airtable = require('airtable');
+
+const airtableConfig = {
+  apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
+  baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+};
+
+const base = new Airtable({ apiKey: airtableConfig.apiKey })
+  .base(airtableConfig.baseKey);
+
+function WordTermDisplay() {
+  const [definitions, setDefinitions] = useState([]);
+
+  const getWordInfo = () => {
+    base('Definition').select({ view: 'Grid view' }).all()
+      .then((records) => {
+        setDefinitions(records);
+      });
+  };
+
+  useEffect(getWordInfo, []);
+
+  return definitions.map((wordInfo) => (
     <div>
-      <h1 className="headings">Dictionary</h1>
-      <WordTermDisplay />
+      <WordTerm
+        word={wordInfo.fields.word !== undefined ? wordInfo.fields.word : 'MISSING WORD'}
+        def={wordInfo.fields.definition !== undefined ? wordInfo.fields.definition : 'MISSING DEFINTION'}
+        links={wordInfo.fields.links !== undefined ? wordInfo.fields.links : 'MISSING LINK'}
+      />
     </div>
-  );
+  ));
 }
 
-export default Dictionary;
+export default WordTermDisplay;
