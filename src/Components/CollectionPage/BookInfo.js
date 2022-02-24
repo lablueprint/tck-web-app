1e55ccb6f4816701d15dde7f38f1703d104fcfd8
Caroline DebBaruah <carolinedebbaruah@MacBook-Pro-2.local>
2022-02-23 23:24:15 -0800

    intial book display

--- src/Components/CollectionPage/BookInfo.js
+++ src/Components/CollectionPage/BookInfo.js
@@ -1,13 +1,7 @@
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
-import Typography from '@mui/material/Typography';
-import './BookInfo.css';
-import { makeStyles } from '@mui/styles';
-import { CardActionArea, CardMedia } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import propTypes from 'prop-types';
+import AuthoredWorkCard from './AuthoredWorkCard';
 
-// airtable configuration
 const Airtable = require('airtable');
 
 const airtableConfig = {
@@ -15,74 +9,55 @@ const airtableConfig = {
   baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
 };
 
-const base = new Airtable({ apiKey: airtableConfig.apiKey })
-  .base(airtableConfig.baseKey);
+const base = new Airtable({ apiKey: airtableConfig.apiKey }).base(airtableConfig.baseKey);
 
-const useStyles = makeStyles({
-  title: {
-    gridRowStart: 3,
-    fontSize: 14,
-    fontWeight: 'bold',
-    paddingBottom: '5px',
-  },
+// Authored and illustrated work components
+function BookInfo({ authorId }) {
+  const [authoredWorks, setAuthoredWorks] = useState([]);
 
-  author: {
-    gridRowStart: 4,
-    fontSize: 12,
-    fontWeight: 100,
-  },
-});
-
-export default function BookInfo({
-  id, title, author, image,
-}) {
-  const classes = useStyles();
-
-  const [authorVar, setAuthor] = useState();
-
-  const getAuthor = () => {
-    base('Creator').find(author, (err, record) => {
-      if (err) { console.error(err); }
-      setAuthor(record);
+  function FindPosts() {
+    const id = authorId;
+    base('Collection').find(id, (err, records) => {
+      if (err) {
+        console.error(err);
+      }
+      const bookid = records.fields.books;
+      if (bookid.length) {
+        bookid.forEach((element) => {
+          base('Book').find(element, (error, record) => {
+            if (error) {
+              console.error(err);
+            }
+            setAuthoredWorks((prevValue) => prevValue.concat(
+              {
+                image: record.fields.image[0].thumbnails.large.url,
+                title: record.fields.title,
+                id: element,
+              },
+            ));
+          });
+        });
+      }
     });
-  };
+  }
 
-  useEffect(getAuthor, []);
+  useEffect(() => {
+    FindPosts();
+  }, []);
 
-  return ( // horizontal scroll not implemented
-    <Card className="card" sx={{ maxWidth: 345 }}>
-      <Link class="link" to={`/book/${id}`} target="_blank">
-        <CardActionArea className="cardActionArea">
-          <div className="cover">
-            <CardMedia
-              className="image"
-              component="img"
-              image={image}
-              alt="missing_book_cover"
-            />
-          </div>
-
-          <Typography className={classes.title}>
-            {title}
-          </Typography>
-          <Typography className={classes.author} color="text.secondary">
-            By
-            {' '}
-            {authorVar !== undefined ? (authorVar.fields.name) : author}
-          </Typography>
-        </CardActionArea>
-      </Link>
-    </Card>
+  return (
+    <div>
+      {authoredWorks.length && <div> Books in this collection: </div>}
+      <div>
+        {authoredWorks.map((element) => (
+          <AuthoredWorkCard key={element.id} image={element.image} title={element.title} />
+        ))}
+      </div>
+      <div />
+    </div>
   );
 }
 
-BookInfo.defaultProps = {
-  image: '',
-};
-
 BookInfo.propTypes = {
-  id: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  image: PropTypes.string,
-};
+  authorId: propTypes.string.isRequired,
+}; export default BookInfo;
