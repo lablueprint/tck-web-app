fac4970d3fc68175b14e7b4a853787693cb93162
Dan Lee <danlee01@ucla.edu>
2022-02-25 16:45:44 -0800

    clean up for pr

--- src/Components/bookHub/BookHub.js
+++ src/Components/bookHub/BookHub.js
@@ -13,25 +13,11 @@ const airtableConfig = {
 const base = new Airtable({ apiKey: airtableConfig.apiKey })
   .base(airtableConfig.baseKey);
 
-/*
-  NOTES:
-    - inefficient lookup of a Creator's authored/illustrated
-      - maybe put all books into a some data structure that has fast lookup and pass it
-        to searchByTerm???
-          - books is a Map<bookId, book> instead of Array
-      - hard to see the current algorithm will be too slow for our purposes
-         without having a big amount of data alr
-
-  <SearchBar setSearchTerms={setSearchTerms}>
-
-  */
 function CardsDisplay() {
   const [books, setBooks] = useState([]);
   const [cards, setCards] = useState([]);
   const [searchTerms, setSearchTerms] = useState(''); // searchTerms should be all lowercase !!
   const [defaultSearch, setDefaultSearch] = useState(true);
-
-  // const [matchedBooks, setMatchedBooks] = useState([]);
 
   const getCards = () => {
     base('Book').select({ view: 'Grid view' }).all()
@@ -39,17 +25,6 @@ function CardsDisplay() {
         setBooks(records);
       });
   };
-
-  /*
-  const getCreator = async (tableName, entryId) => new Promise((resolve, reject) => {
-    base(tableName).find(entryId, (err, creatorRecord) => {
-      if (err) {
-        reject();
-      }
-      resolve(creatorRecord);
-    });
-  });
-  */
 
   const isMatch = (book) => {
     // Filter function for books stored in 'books' state
@@ -84,37 +59,12 @@ function CardsDisplay() {
   const Filter = (table, field) => new Promise((resolve, reject) => {
     // Query Airtable {table} for records whose {field} value matches the search term
     // This will mainly be for Creator table
-    /*
-    base(table).select({
-      filterByFormula: `IF(FIND(LOWER("${searchTerms}"), LOWER(name)) != 0, ${field}, '')`,
-      maxRecords: 3,
-      view: 'Grid view',
-    }).eachPage((records, fetchNextPage) => {
-      records.forEach((record) => {
-        const bookId = record.get('authored'); // array of strings (bookId)
 
-        for (let i = 0; i < bookId.length; i += 1) {
-          for (let j = 0; j < books.length; j += 1) {
-            if (bookId[i] === books[j].get('id')) {
-              // newArr = [...oldArr, x]
-              if (!matchedBooks.includes(books[j])) {
-                setMatchedBooks([...matchedBooks, books[j]]);
-              }
-            }
-          }
-        }
-      });
-      fetchNextPage();
-    }, (err) => {
-      if (err) { reject(err); }
-    });
-    */
     base(table).select({
       filterByFormula: `IF(FIND(LOWER("${searchTerms}"), LOWER(name)) != 0, ${field}, '')`,
       view: 'Grid view',
     }).all()
       .then((records) => {
-        console.log(records);
         const res = [];
         records.forEach((record) => {
           const bookIds = record.get('authored'); // array of strings (bookId)
@@ -144,41 +94,14 @@ function CardsDisplay() {
       // remove duplicates
       matched = [...new Set(matched)];
     }
-    // setMatchedBooks(matched);
-    console.log(matched);
+
     setCards(matched);
-
-    /*
-    base('Creator').select({
-      // Selecting the first 3 records in Grid view:
-      filterByFormula: `IF(FIND(LOWER("${searchTerms}"), LOWER(name)) != 0, authored, '')`,
-      maxRecords: 3,
-      view: 'Grid view',
-    }).eachPage((records, fetchNextPage) => {
-      // This function (`page`) will get called for each page of records.
-
-      // Records is array of Creators whose names match the searchTerms
-      records.forEach((record) => {
-        console.log('Retrieved', record.get('id'));
-        console.log(record.get('name'));
-      });
-
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      fetchNextPage();
-    }, (err) => {
-      if (err) { console.error(err); }
-    });
-    console.log('EXT');
-    */
   };
 
   useEffect(() => {
     if (!books.length) { getCards(); }
     if (searchTerms) {
       (async () => searchByTerm())();
-      // searchByTerm();
     } else {
       setCards(books);
     }
@@ -208,3 +131,13 @@ function CardsDisplay() {
 }
 
 export default CardsDisplay;
+
+/*
+  NOTES:
+    - inefficient lookup of a Creator's authored/illustrated
+      - maybe put all books into a some data structure that has fast lookup and pass it
+        to searchByTerm???
+          - books is a Map<bookId, book> instead of Array
+      - hard to see the current algorithm will be too slow for our purposes
+         without having a big amount of data alr
+*/
