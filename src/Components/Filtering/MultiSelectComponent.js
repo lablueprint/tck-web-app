47cba2fe61a64a592f2d5e0202fc0f47c59304b8
soliavalentine <43827643+soliavalentine@users.noreply.github.com>
2022-03-02 20:55:38 -0800

    Update src/Components/Filtering/MultiSelectComponent.js
    Co-authored-by: DiyaBaliga <38540740+DiyaBaliga@users.noreply.github.com>

--- src/Components/Filtering/MultiSelectComponent.js
+++ src/Components/Filtering/MultiSelectComponent.js
@@ -13,7 +13,6 @@ export default function MultiselectComponent({
 
   const handleToggle = (value, label, checked) => {
     if (checked) {
-      // flag = 1;
       setInput({ ...input, [label]: input[label].concat(value) });
     } else {
       setInput({ ...input, [label]: input[label].filter((element) => element !== value) });
