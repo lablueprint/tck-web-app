4f99583bcd6bae3ae9ff7d288269d45b76541ea4
Dan Lee <danlee01@ucla.edu>
2022-03-28 23:26:19 -0700

    scaffording

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {
   Route, Routes,
 } from 'react-router-dom';
-import { Dictionary, Home } from './Pages';
+// import { Dictionary, Home } from './Pages';
+import { Dictionary } from './Pages';
 import Header from './Components';
 import './App.css';
 import CreatorPage from './Pages/CreatorPage';
 import CollectionPage from './Pages/CollectionPage';
+
+// TEST
+import BookBrowser from './Components/BookBrowser/BookBrowser';
 
 import BookPage from './Pages/BookPage';
 
@@ -15,7 +19,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<BookBrowser />} />
         <Route path="/dictionary" element={<Dictionary />} />
         <Route exact path="/book/:bookId" element={<BookPage />} />
         <Route exact path="/creator/:id" element={<CreatorPage />} />
