60876a896c3c2fd1c31680aa6f14fe88edec8223
Ryan Yeo <ryandc.yeo@gmail.com>
2022-04-04 22:34:28 -0700

    Implemented filter function

--- src/Components/Recommendations/BookRec.js
+++ src/Components/Recommendations/BookRec.js
@@ -1,7 +1,12 @@
 /*
     ============ TODO =============
-    1. Create similar bookHub book books that are recommended.
-    2. Figure out filters.
+    - Get current book's tags
+    - query airtable for a list of similar books
+    - store the list somewhere
+
+    Prioritization:
+      primary -> age-range, grade-range, race/ethnicity, and identity
+      secondary -> author,  genre, theme/lessons
 
     Filter:
       must make sure that the same book is not shown
@@ -9,6 +14,7 @@
 */
 
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import BookCards from './BookRecCards';
 
 // airtable configuration
@@ -22,23 +28,61 @@ const airtableConfig = {
 const base = new Airtable({ apiKey: airtableConfig.apiKey })
   .base(airtableConfig.baseKey);
 
-function CardsDisplay() {
-  const [cards, setCards] = useState([]);
+export default function RecCardsDisplay() {
+  const [book, setBook] = useState([]);
+  // const [ageRange, setAgeRange] = useState([]);
+  // const [gradeRange, setGradeRange] = useState([]);
+  /*
+  const [ethnicity, setEthnicity] = useState([]); */
+  // const [identity, setIdentity] = useState();
+  // const [filteredRec, setFilteredRec] = useState([]);
 
-  const getCards = () => {
-    base('Book').select({ view: 'Grid view' }).all()
-      .then((records) => {
-        setCards(records);
+  // const getCards = () => {
+  //   base('Book').select({ view: 'Grid view' }).all()
+  //     .then((records) => {
+  //       setBook(records);
+  //     });
+  // };
+
+  // Instead of using props, we pull bookId from URL
+  const { bookId } = useParams();
+
+  // Airtable Filter implementation
+  function RecFilter(field, fieldKeyword) {
+    base('Book').select({
+      filterByFormula: `SEARCH("${fieldKeyword}", {${field}})`,
+      maxRecords: 3,
+      view: 'Grid view',
+    }).eachPage((records, fetchNextPage) => {
+    // This function (`page`) will get called for each page of records.
+      setBook(records);
+      records.forEach((record) => {
+        console.log('Retrieved', record.get('id'));
+        // setBook(record);
       });
-  };
+      console.log(fieldKeyword);
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+    }, (err) => {
+      if (err) { console.error(err); }
+    });
+  }
 
-  useEffect(getCards, []);
+  // console.log(book);
+  // console.log(ageRange);
+
+  useEffect(() => {
+    // getCards();
+    RecFilter('id', bookId);
+  }, []);
 
   return (
     <div>
       <h2>Books Like This</h2>
       <div className="library-display">
-        {cards.map((card) => (
+        {book.map((card) => (
           <BookCards
             key={card.id}
             id={card.id}
@@ -51,5 +95,3 @@ function CardsDisplay() {
     </div>
   );
 }
-
-export default CardsDisplay;
