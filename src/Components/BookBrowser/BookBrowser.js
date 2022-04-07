2d0cdb6e3ebdd990634570d6c8aa689f62c11d9b
Dan Lee <danlee01@ucla.edu>
2022-04-06 21:03:07 -0700

    update

--- src/Components/BookBrowser/BookBrowser.js
+++ src/Components/BookBrowser/BookBrowser.js
@@ -1,12 +1,17 @@
-/* eslint-disable */
 import React, { useState } from 'react';
 import {
   ToggleButton, ToggleButtonGroup,
 } from '@mui/material';
+import CardsDisplay from '../bookHub/BookHub';
+import SearchBar from '../SearchBar/SearchBar';
 
 import './BookBrowser.css';
+
 function BookBrowser() {
   const [alignment, setAlignment] = useState('Search');
+
+  const [searchTerms, setSearchTerms] = useState('');
+  const [defaultSearch, setDefaultSearch] = useState(true);
 
   const handleChange = (event, newAlignment) => {
     if (newAlignment !== null) {
@@ -15,52 +20,35 @@ function BookBrowser() {
   };
 
   return (
-    <div className="Browser">
-      <div className="BrowserHead">
-        <div className="BrowserQuote">Browse For Books</div>
-        <ToggleButtonGroup
-          color="primary"
-          value={alignment}
-          exclusive
-          onChange={handleChange}
-          sx={{ margin: '3vh 3vw auto' }}
-        >
-          <ToggleButton value="Search">Search</ToggleButton>
-          <ToggleButton value="Filter">Filter</ToggleButton>
-        </ToggleButtonGroup>
-      </div>
-      <div className="BrowserBody">
-        {(alignment) ? `I AM ${alignment}ING` : 'no thang'}
-        DSfdsfSDFDSFWEFDFdsfEWFEFERFSDFSDF
+    <div id="background">
 
-        searchingf
-        {'\n'}
-        defaultf
-        {'\n'}
-        DSfdsfSDFDSFWEFDFdsfEWFEFERFSDFSDFfs
-        {'\n'}
-        defaultsd
-        {'\n'}
-        fields
-        searchingf
-        {'\n'}
-        defaultf
-        {'\n'}
-        DSfdsfSDFDSFWEFDFdsfEWFEFERFSDFSDFfs
-        {'\n'}
-        defaultsd
-        {'\n'}
-        fields
-        searchingf
-        {'\n'}
-        defaultf
-        {'\n'}
-        DSfdsfSDFDSFWEFDFdsfEWFEFERFSDFSDFfs
-        {'\n'}
-        defaultsd
-        {'\n'}
-        fields
+      <div className="Browser">
+        <div className="BrowserHead">
+          <div className="BrowserQuote">Browse For Books</div>
+          <ToggleButtonGroup
+            color="primary"
+            value={alignment}
+            exclusive
+            onChange={handleChange}
+            sx={{ margin: '3vh 3vw auto' }}
+          >
+            <ToggleButton value="Search">Search</ToggleButton>
+            <ToggleButton value="Filter">Filter</ToggleButton>
+          </ToggleButtonGroup>
+        </div>
+        <div className="BrowserBody">
+          {
+          (alignment === 'Search')
+            ? <SearchBar setSearchTerms={setSearchTerms} setDefaultSearch={setDefaultSearch} />
+            : <p> filtering parameters to comeee</p>
+        }
+
+        </div>
       </div>
+      {
+          (alignment === 'Search') ? <CardsDisplay searchTerms={searchTerms} defaultSearch={defaultSearch} />
+            : <p>filtering n shet</p>
+      }
     </div>
   );
 }
@@ -77,8 +65,19 @@ export default BookBrowser;
  *        Search bar
  *      Filter
  *        ranges, checks, whateva
- *      
+ *
  *  ?? Results body ???
+ *
+ *
+ *  TO-DO
+ *  ------
+ *    1. need to separate search bar from results
+ *        Pass searchTerms and defaultSearch to CardsDisplay as props rather than having it as state
+ *          <SearchBar setSearchTerms={setSearchTerms} setDefaultSearch={setDefaultSearch} />
+ *          <CardsDisplay searchTerms={searchTerms} defaultSearch={defaultSearch}
+ *        DONE
+ *    2. Use JS and CSS instead of MaterialUI/ToggleButton to allow for easier styling??
+ *       Or figure out how to customize the ToggleButton so it matches the Figma
  *
  *  COMPROMISES
  *   Neue Haas Grotesk Text Pro -> IBM PLEX
