5ef31c6d2c99a83e03e0bec74cce0f2731f901f9
Dan Lee <danlee01@ucla.edu>
2022-04-06 21:47:40 -0700

    added Filtering

---
+++ src/Components/Filtering/Filtering.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import propTypes from 'prop-types';
+import RangeFilter from './RangeFilter';
+import MultSelectElem from './MultiselectFilters';
+
+const gradeRangeMetadata = ['0 to Pre-K', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th'];
+const ageRangeMetadata = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+
+export default function Filter({ setRangeState, setMultiSelectInput }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [tempRangeFilterData, setTempRangeFilterData] = useState({
+    age:
+    { min: ageRangeMetadata[0], max: ageRangeMetadata[18] },
+    grade: { min: gradeRangeMetadata[0], max: gradeRangeMetadata[12] },
+  });
+  const [tempMultiSelect, setTempMultiSelect] = useState({
+    Ethnicity: [],
+    Religion: [],
+    Gender: [],
+    Sexuality: [],
+  });
+  const open = Boolean(anchorEl);
+
+  const handleSave = () => {
+    setRangeState(tempRangeFilterData);
+    setMultiSelectInput(tempMultiSelect);
+  };
+  const handleClick = (event) => {
+    // console.log(tempRangeFilterData);
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', rowGap: 100 }}>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Filters
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem>
+          <RangeFilter
+            rangeFilterData={tempRangeFilterData}
+            setRangeFilterData={setTempRangeFilterData}
+          />
+
+        </MenuItem>
+        <MenuItem>
+          <MultSelectElem
+            setTempMultiSelect={setTempMultiSelect}
+            tempMultiSelect={tempMultiSelect}
+          />
+
+        </MenuItem>
+        <MenuItem>
+          <button
+            type="button"
+            onClick={handleSave}
+          >
+            Save
+          </button>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+Filter.propTypes = {
+  setMultiSelectInput: propTypes.func.isRequired,
+  setRangeState: propTypes.func.isRequired,
+};
---
+++ src/Components/Filtering/RangeFilterCard.js
@@ -0,0 +1,59 @@
+import React, { useRef } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import propTypes from 'prop-types';
+
+function RangeFilterCard({
+  filterTitle, optionsArray, handleChange, data,
+}) {
+  const minref = useRef();
+  const maxref = useRef();
+  return (
+    <div>
+      <p>{filterTitle}</p>
+      <div style={{ display: 'flex', flexDirection: 'row', columnGap: 50 }}>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          ref={minref}
+          options={optionsArray}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Min" />}
+          onChange={(event, newValue) => { handleChange(minref.current.getAttribute('name'), newValue, event); }}
+          defaultValue={filterTitle === 'Age' ? data.age.min : data.grade.min}
+          name={filterTitle === 'Age' ? 'Age-min' : 'Grade-min'}
+        />
+        <p>to</p>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          ref={maxref}
+          options={optionsArray}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Max" />}
+          onChange={(event, newValue) => { handleChange(maxref.current.getAttribute('name'), newValue, event); }}
+          defaultValue={filterTitle === 'Age' ? data.age.max : data.grade.max}
+          name={filterTitle === 'Age' ? 'Age-max' : 'Grade-max'}
+        />
+      </div>
+    </div>
+  );
+}
+
+RangeFilterCard.propTypes = {
+  filterTitle: propTypes.string,
+  handleChange: propTypes.func.isRequired,
+  data: propTypes.shape(
+    {
+      age: propTypes.shape({ min: propTypes.string.isRequired, max: propTypes.string.isRequired }),
+      grade: propTypes.shape({ min: propTypes.string.isRequired, max: propTypes.string }),
+    },
+  ).isRequired,
+  optionsArray: propTypes.arrayOf(propTypes.string),
+};
+
+RangeFilterCard.defaultProps = {
+  filterTitle: '',
+  optionsArray: [],
+};
+export default RangeFilterCard;
