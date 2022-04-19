5c17f660d450e87b7e31e280813906e2ef4a5cac
Caroline DebBaruah <carolinedebbaruah@MacBook-Pro-2.local>
2022-04-18 22:15:34 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './Components';
 import './App.css';
 import CreatorPage from './Pages/CreatorPage';
 import CollectionPage from './Pages/CollectionPage';
+import Quiz from './Components/Quiz/quiz';
 
 import BookPage from './Pages/BookPage';
 
@@ -20,6 +21,7 @@ function App() {
         <Route exact path="/book/:bookId" element={<BookPage />} />
         <Route exact path="/creator/:id" element={<CreatorPage />} />
         <Route exact path="/collection/:id" element={<CollectionPage />} />
+        <Route exact path="/quiz" element={<Quiz />} />
       </Routes>
     </div>
   );
---
+++ src/Components/Quiz/quiz.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Quiz() {
+  return (
+    <h1>
+      The Conscious Kid Reccomendation Quiz
+    </h1>
+  );
+}
