55c3742c0dcdcc4029b7b868dc411b9806da6462
Dan Lee <danlee01@ucla.edu>
2022-04-24 14:57:02 -0700

    implement 'all' search

--- src/Pages/BrowserPage.js
+++ src/Pages/BrowserPage.js
@@ -8,3 +8,8 @@ function BrowserPage() {
 }
 
 export default BrowserPage;
+
+/** NOTES
+ * This page may seem unnecessary for now, but will provide an clearer way
+ * to combine with other components like Collections later down the line.
+ */
