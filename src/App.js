7cff0b16f260886462372f2047af5d555cf66e11
Diya Baliga <baligadiya@gmail.com>
2022-04-25 18:15:43 -0700

    Code Cleanup: File reorganization and dictionary debugging

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from 'react';
 import {
   Route, Routes,
 } from 'react-router-dom';
-import { Dictionary, Home } from './Pages';
+import {
+  Dictionary, Home, CreatorPage, BookPage, CollectionPage,
+} from './Pages';
 import Header from './Components';
 import './App.css';
-import CreatorPage from './Pages/CreatorPage';
-import CollectionPage from './Pages/CollectionPage';
-import DictionaryTitle from './Components/Dictionary/DictionaryTitle';
-
-import BookPage from './Pages/BookPage';
 
 function App() {
   return (
@@ -17,7 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dictionary" element={[<DictionaryTitle />, <Dictionary />]} />
+        <Route path="/dictionary" element={<Dictionary />} />
         <Route exact path="/book/:bookId" element={<BookPage />} />
         <Route exact path="/creator/:id" element={<CreatorPage />} />
         <Route exact path="/collection/:id" element={<CollectionPage />} />
