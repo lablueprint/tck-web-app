807c8b7c0924f536155578110519e3757552204f
Dan Lee <danlee01@ucla.edu>
2022-04-28 13:54:28 -0700

    clean up

--- src/Components/BookList/ListMenu.js
+++ src/Components/BookList/ListMenu.js
@@ -1,85 +1,97 @@
-/* eslint-disable */
 import React, { useState } from 'react';
-import {Button, Menu, MenuItem, ListItemText, ListItemIcon, Fade} from '@mui/material';
+import PropTypes from 'prop-types';
+import {
+  Button, Menu, MenuItem, ListItemText, ListItemIcon, Fade,
+} from '@mui/material';
 import { Check } from '@mui/icons-material';
 
 const BUTTON_SX = {
-    textTransform: 'none', 
-    borderRadius: '10px',
-    margin: '2vh 0 1vh 1vw'
+  textTransform: 'none',
+  borderRadius: '10px',
+  margin: '2vh 0 1vh 1vw',
+  fontFamily: 'Work Sans',
+  fontWeight: '600',
 };
 
+function ListMenu({
+  menuText, menuIcon, options, value, handleChange,
+}) {
+  // Pop-up
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-function ListMenu({ menuText, menuIcon, options, value, handleChange }) {
-    // Pop-up
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = (event) => {
-        setAnchorEl(null);
-    };
+  const handleChoose = (event) => {
+    handleChange(event.currentTarget.value);
+  };
 
-    const handleChoose = (event) => {
-        console.log(event.currentTarget.value);
-        handleChange(event.currentTarget.value);
-
-    };
-
-    const menuItems = options.map((option) => { return (
-        <MenuItem key={option.id} value={option.value} onClick={handleChoose} >
-            {
-                (option.icon) ? <ListItemIcon>{option.icon}</ListItemIcon> : <div/>
+  const menuItems = options.map((option) => (
+    <MenuItem
+      key={option.id}
+      value={option.value}
+      onClick={handleChoose}
+      sx={{ minWidth: '15vw', fontFamily: 'Work Sans', fontWeight: '600' }}
+    >
+      {
+                (option.icon) ? <ListItemIcon>{option.icon}</ListItemIcon> : <div />
             }
-            <ListItemText>{option.text}</ListItemText>
-            {
-                (value === option.value) ? <ListItemIcon><Check/></ListItemIcon> : <div/>
+      <ListItemText>{option.text}</ListItemText>
+      {
+                (value === option.value) ? <ListItemIcon><Check /></ListItemIcon> : <div />
             }
-        </MenuItem>
-        );
-    });
+    </MenuItem>
+  ));
 
-    return (
-        <div>
-            <Button 
-                sx={BUTTON_SX}
-                id='basic-button'
-                variant="outlined"
-                onClick={handleClick}
-                endIcon={menuIcon}
-                size="small"
-            >
-                {menuText}
-            </Button>
-            <Menu
-                id='basic-menu'
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                TransitionComponent={Fade}
-                MenuListProps={{
-                    'onClick': handleClose,
-                }}
-            >
-                { menuItems }
+  return (
+    <div>
+      <Button
+        sx={BUTTON_SX}
+        id="basic-button"
+        variant="outlined"
+        onClick={handleClick}
+        endIcon={menuIcon}
+        size="small"
+      >
+        {menuText}
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+        MenuListProps={{
+          onClick: handleClose,
+        }}
+      >
+        { menuItems }
 
-            </Menu>
-        </div>
-    );
-
-};
+      </Menu>
+    </div>
+  );
+}
 
 export default ListMenu;
 
+ListMenu.propTypes = {
+  menuText: PropTypes.string.isRequired,
+  menuIcon: PropTypes.elementType.isRequired,
+  options: PropTypes.PropTypes.arrayOf(PropTypes.elementType).isRequired,
+  value: PropTypes.number.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
 /*
-<ListMenu 
-    Label={Component} 
-    options={array of options for IconItemIcon, IconItemText} 
+<ListMenu
+    Label={Component}
+    options={array of options for IconItemIcon, IconItemText}
     handleChange={setBooksPerPage, setSortBy}
     value={value}  // seeing which one to add check mark to
 />
-
-
 
 */
