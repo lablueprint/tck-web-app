8f31f9868b91f5c42a734dad19d49ab8b48cac76
ascend2001 <avnishsengupta1@g.ucla.edu>
2022-04-30 19:11:40 -0700

    updated footer collection link

--- src/Components/Navigation/Footer.js
+++ src/Components/Navigation/Footer.js
@@ -26,7 +26,7 @@ function Footer() {
           Home
         </NavLink>
         <NavLink
-          to="/"
+          to="/collection/init"
           className="menu-options"
         >
           Collections
