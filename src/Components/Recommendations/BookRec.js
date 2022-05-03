83e94aa5326a57bedfcbb070b694bf283ec0418d
Ryan Yeo <ryandc.yeo@gmail.com>
2022-05-02 22:58:03 -0700

    Entire implementation completed. Did not use async, but instead used React hooks to resolve the issue. Implementation even works for the UI. Cleaned up code, changed Airtable to fetch all records at once instead of pagination. (Note to improve algorithm: Could improve the algorithm by NOT having to create an extra list to iterate through.)

--- src/Components/Recommendations/BookRec.js
+++ src/Components/Recommendations/BookRec.js
@@ -1,33 +1,3 @@
-/*
-    ============ TODO =============
-    - Get current book's tags
-    - query airtable for a list of similar books
-    - store the list somewhere
-
-    Prioritization:
-      primary -> age-range, grade-range, race/ethnicity, and identity
-      secondary -> author,  genre, theme/lessons
-
-    Filter:
-      must make sure that the same book is not shown
-
-    How to make sure prioritization is in the correct range:
-      - priorisation: books that have the most correct matches to the top tier.
-      - top tier: age_range, grade_range, race/ethnicity
-      - if books have same number of correct matches,
-        prioritize age_range > grade_range > race_ethnicity
-
-    Challenges:
-      - Tweak filter that can work with array of data
-      - Tweak filter to work with range of values
-
-    If airtable filter doesn't work:
-      fetch all books from airtable
-      apply filters and increment the priority counter for each book accordingly
-      sort based on priority and display the top n books
-
-*/
-
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -45,68 +15,39 @@ const base = new Airtable({ apiKey: airtableConfig.apiKey })
   .base(airtableConfig.baseKey);
 
 export default function RecCardsDisplay({
-  minAge, maxAge, minGrade, maxGrade, raceEthnicity,
+  minAge, maxAge, minGrade, maxGrade, raceEthnicity, genre,
 }) {
-  const [book, setBook] = useState([]);
   const gradeList = ['0 to Pre-K', 'Kindergarten', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th'];
   const prioMap = new Map();
   const [recList, setRecList] = useState([]);
-  // const [ageRange, setAgeRange] = useState([]);
-  // const [gradeRange, setGradeRange] = useState([]);
-  /*
-  const [ethnicity, setEthnicity] = useState([]); */
-  // const [identity, setIdentity] = useState();
-  // const [filteredRec, setFilteredRec] = useState([]);
-
-  // const getCards = () => {
-  //   base('Book').select({ view: 'Grid view' }).all()
-  //     .then((records) => {
-  //       setBook(records);
-  //     });
-  // };
 
   // Instead of using props, we pull bookId from URL
   const { bookId } = useParams();
 
-  // Airtable Filter implementation
-  // filterByFormula: `IF(${field} = {id},
-  // SEARCH("search for age_range", "id"), IF("", "", SEARCH("${fieldKeyword}", {${field}})))`
-  /*
-IF(
-        ${field} = {id},
-        SEARCH("search for age_range", "id"),
-        IF("", "", SEARCH("${fieldKeyword}", {${field}}))
-      )
-  */
-  // console.log({ minAge });
-  // console.log({ maxAge });
-  // console.log({ minGrade });
-  // console.log({ maxGrade });
-  // console.log({ raceEthnicity });
-
-  function RecFilter(field, fieldKeyword) {
-    // const map1 = new Map();
+  function RecFilter() {
     base('Book').select({
       view: 'Grid view',
-    }).eachPage((records, fetchNextPage) => {
-      setBook(records);
+    }).all().then((records) => {
       records.forEach((record) => {
         // do not check with the identical book
         if (bookId !== record.get('id')) {
           let priority = 0;
-          if ((minAge <= record.fields.age_max) && (maxAge >= record.fields.age_min)) {
-            priority += 2;
-          }
-
           const minGradeIndex = gradeList.indexOf(minGrade, 0);
           const maxGradeIndex = gradeList.indexOf(maxGrade, 0);
           const minGradeIndexRecord = gradeList.indexOf(record.fields.grade_min, 0);
           const maxGradeIndexRecord = gradeList.indexOf(record.fields.grade_max, 0);
 
+          // Matching age range gives 2 points
+          if ((minAge <= record.fields.age_max) && (maxAge >= record.fields.age_min)) {
+            priority += 2;
+          }
+
+          // Matching grade range gives 2 points
           if ((minGradeIndex <= maxGradeIndexRecord) && (maxGradeIndex >= minGradeIndexRecord)) {
             priority += 2;
           }
 
+          // Matching race/ethnicity gives 1 point each
           if (raceEthnicity && record.fields['race/ethnicity']) {
             for (let i = 0; i < raceEthnicity.length; i += 1) {
               if (record.fields['race/ethnicity'].includes(raceEthnicity[i])) {
@@ -114,54 +55,34 @@ IF(
               }
             }
           }
-          // console.log(record.get('id'), priority);
-          /*
-          race
-          identity
-          genre
-        */
+
+          // Matching genre gives 1 point each
+          if (genre && record.fields.genre) {
+            for (let i = 0; i < genre.length; i += 1) {
+              if (record.fields.genre.includes(genre[i])) {
+                priority += 1;
+              }
+            }
+          }
+
           // Storing book ID based on its priority in a Map data structure
           // Key is priority; Value is an array of corresponding bookIDs
           if (prioMap.has(priority)) {
             const array = [];
             const prioList = array.concat(prioMap.get(priority));
-            const newPrioList = prioList.concat(record.get('id'));
+            const newPrioList = prioList.concat(record);
             prioMap.set(priority, newPrioList);
           } else {
-            prioMap.set(priority, record.get('id'));
+            prioMap.set(priority, record);
           }
-          // console.log(prioMap);
-          /*
-          // console.log(record.fields['race/ethnicity']);
-          if (record.fields['race/ethnicity'] === undefined) {
-            console.log('Race/Ethnicity is undefined');
-          } else if (prioMap.has(priority)) {
-            const prioList = prioMap.get(priority);
-            let newPrioList = prioList;
-            for (let i = 0; i < record.fields['race/ethnicity'].length; i += 1) {
-              if (!prioList.includes(record.fields['race/ethnicity'][i])) {
-                newPrioList = prioList.concat(record.fields['race/ethnicity'][i]);
-              }
-            }
-            prioMap.set(priority, newPrioList);
-          } else {
-            prioMap.set(priority, record.fields['race/ethnicity']);
-          }
-          // console.log(prioMap);
-          console.log('.');
-          */
         }
       });
-      console.log(fieldKeyword);
-      console.log(bookId);
-      console.log(prioMap);
       const sortedArr = [];
       prioMap.forEach((value, key) => {
         sortedArr.push(key);
       });
       sortedArr.sort();
       sortedArr.reverse();
-      console.log(sortedArr);
 
       const finalArr = [];
       let counter = 0;
@@ -183,37 +104,25 @@ IF(
           counter += 1;
         }
       }
-      console.log(finalArr);
-      setRecList(finalArr); // This part does not get updated due to lack of async
-      console.log(recList);
-      /*
-      book.forEach((record) => {
-
-      });
-      */
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      fetchNextPage();
+      setRecList(finalArr);
     }, (err) => {
       if (err) { console.error(err); }
     });
   }
 
-  // console.log(book);
-  // console.log(ageRange);
-
+  // Call the function upon first render
   useEffect(() => {
-    // getCards();
-    RecFilter('id', bookId);
-    // console.log(curBook.record);
+    RecFilter();
   }, []);
+
+  // Re-render once if recList has changed
+  useEffect(() => {}, [recList]);
 
   return (
     <div>
       <h2>Books Like This</h2>
       <div className="library-display">
-        {book.map((card) => (
+        {recList.map((card) => (
           <BookCards
             key={card.id}
             id={card.id}
@@ -233,6 +142,7 @@ RecCardsDisplay.propTypes = {
   minGrade: PropTypes.string,
   maxGrade: PropTypes.string,
   raceEthnicity: PropTypes.string,
+  genre: PropTypes.string,
 };
 
 RecCardsDisplay.defaultProps = {
@@ -241,4 +151,5 @@ RecCardsDisplay.defaultProps = {
   minGrade: '0',
   maxGrade: '0',
   raceEthnicity: '0',
+  genre: '0',
 };
