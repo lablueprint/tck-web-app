b992bdb6a8d19d73f973a13c9cca690276a48652
Ryan Yeo <ryandc.yeo@gmail.com>
2022-05-11 22:36:41 -0700

    Implemented async to allow function to be used to other purposes

--- src/Components/Recommendations/BookRec.js
+++ src/Components/Recommendations/BookRec.js
@@ -22,92 +22,94 @@ export default function RecFilter(
   raceEthnicity,
   genre,
 ) {
-  const prioMap = new Map();
-  const finalArr = [];
-
-  base('Book').select({
-    view: 'Grid view',
-  }).all().then((records) => {
-    records.forEach((record) => {
+  const filter = () => new Promise((resolve, reject) => {
+    const prioMap = new Map();
+    base('Book').select({
+      view: 'Grid view',
+    }).all().then((records) => {
+      const finalArr = [];
+      records.forEach((record) => {
       // do not check with the identical book
-      if (bookId !== record.get('id')) {
-        let priority = 0;
-        const minGradeIndex = gradeList.indexOf(minGrade, 0);
-        const maxGradeIndex = gradeList.indexOf(maxGrade, 0);
-        const minGradeIndexRecord = gradeList.indexOf(record.fields.grade_min, 0);
-        const maxGradeIndexRecord = gradeList.indexOf(record.fields.grade_max, 0);
+        if (bookId !== record.get('id')) {
+          let priority = 0;
+          const minGradeIndex = gradeList.indexOf(minGrade, 0);
+          const maxGradeIndex = gradeList.indexOf(maxGrade, 0);
+          const minGradeIndexRecord = gradeList.indexOf(record.fields.grade_min, 0);
+          const maxGradeIndexRecord = gradeList.indexOf(record.fields.grade_max, 0);
 
-        // Matching age range gives 2 points
-        if ((minAge <= record.fields.age_max) && (maxAge >= record.fields.age_min)) {
-          priority += 2;
-        }
+          // Matching age range gives 2 points
+          if ((minAge <= record.fields.age_max) && (maxAge >= record.fields.age_min)) {
+            priority += 2;
+          }
 
-        // Matching grade range gives 2 points
-        if ((minGradeIndex <= maxGradeIndexRecord) && (maxGradeIndex >= minGradeIndexRecord)) {
-          priority += 2;
-        }
+          // Matching grade range gives 2 points
+          if ((minGradeIndex <= maxGradeIndexRecord) && (maxGradeIndex >= minGradeIndexRecord)) {
+            priority += 2;
+          }
 
-        // Matching race/ethnicity gives 1 point each
-        if (raceEthnicity && record.fields['race/ethnicity']) {
-          for (let i = 0; i < raceEthnicity.length; i += 1) {
-            if (record.fields['race/ethnicity'].includes(raceEthnicity[i])) {
-              priority += 1;
+          // Matching race/ethnicity gives 1 point each
+          if (raceEthnicity && record.fields['race/ethnicity']) {
+            for (let i = 0; i < raceEthnicity.length; i += 1) {
+              if (record.fields['race/ethnicity'].includes(raceEthnicity[i])) {
+                priority += 1;
+              }
             }
           }
-        }
 
-        // Matching genre gives 1 point each
-        if (genre && record.fields.genre) {
-          for (let i = 0; i < genre.length; i += 1) {
-            if (record.fields.genre.includes(genre[i])) {
-              priority += 1;
+          // Matching genre gives 1 point each
+          if (genre && record.fields.genre) {
+            for (let i = 0; i < genre.length; i += 1) {
+              if (record.fields.genre.includes(genre[i])) {
+                priority += 1;
+              }
             }
           }
-        }
 
-        // Storing book ID based on its priority in a Map data structure
-        // Key is priority; Value is an array of corresponding bookIDs
-        if (prioMap.has(priority)) {
-          const array = [];
-          const prioList = array.concat(prioMap.get(priority));
-          const newPrioList = prioList.concat(record);
-          prioMap.set(priority, newPrioList);
+          // Storing book ID based on its priority in a Map data structure
+          // Key is priority; Value is an array of corresponding bookIDs
+          if (prioMap.has(priority)) {
+            const array = [];
+            const prioList = array.concat(prioMap.get(priority));
+            const newPrioList = prioList.concat(record);
+            prioMap.set(priority, newPrioList);
+          } else {
+            prioMap.set(priority, record);
+          }
+        }
+      });
+      const sortedArr = [];
+      prioMap.forEach((value, key) => {
+        sortedArr.push(key);
+      });
+      sortedArr.sort();
+      sortedArr.reverse();
+
+      let counter = 0;
+      for (let i = 0; i < sortedArr.length; i += 1) {
+        if (counter === 14) {
+          break;
+        }
+        const value = prioMap.get(sortedArr[i]);
+        if (Array.isArray(value)) {
+          for (let j = 0; j < value.length; j += 1) {
+            if (counter === 14) {
+              break;
+            }
+            finalArr.push(value[j]);
+            counter += 1;
+          }
         } else {
-          prioMap.set(priority, record);
-        }
-      }
-    });
-    const sortedArr = [];
-    prioMap.forEach((value, key) => {
-      sortedArr.push(key);
-    });
-    sortedArr.sort();
-    sortedArr.reverse();
-
-    let counter = 0;
-    for (let i = 0; i < sortedArr.length; i += 1) {
-      if (counter === 14) {
-        break;
-      }
-      const value = prioMap.get(sortedArr[i]);
-      if (Array.isArray(value)) {
-        for (let j = 0; j < value.length; j += 1) {
-          if (counter === 14) {
-            break;
-          }
-          finalArr.push(value[j]);
+          finalArr.push(value);
           counter += 1;
         }
-      } else {
-        finalArr.push(value);
-        counter += 1;
       }
-    }
-    console.log('finalArr', finalArr);
-    return finalArr;
-  }, (err) => {
-    if (err) { console.error(err); }
+      console.log('finalArr', finalArr);
+      resolve(finalArr);
+    }, (err) => {
+      if (err) { reject(err); }
+    });
   });
+  return filter();
 }
 
 /*
