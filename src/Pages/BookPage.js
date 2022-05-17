f7ac1dae9b921b10c75fc46b86385ed2c5c0ef3d
Dan Lee <danlee01@ucla.edu>
2022-05-16 21:44:38 -0700

    manual clone

--- src/Pages/BookPage.js
+++ src/Pages/BookPage.js
@@ -1,20 +1,54 @@
+/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import {
-  Paper, Link as LinkUI, List, ListItem, ListItemText,
+  Paper, Box
 } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import BookSynopsis from '../Components/BookPage/BookSynopsis';
+import CollectionsCarousel from '../Components/CollectionsComponents/CollectionsCarousel';
 import Logo from '../Assets/Images/TCK PNG Logo.png';
+import LeftArrow from '../Assets/Images/left-arrow-author-page.svg';
+import RightArrow from '../Assets/Images/right-arrow-author-page.svg';
 
 const Airtable = require('airtable');
 
 const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY })
   .base(process.env.REACT_APP_AIRTABLE_BASE_KEY);
 
+const styles = {
+  iframeContainer: {
+    position: 'relative',
+    overflow: 'hidden',
+    width: '100%',
+    paddingTop: '56.25%',
+    margin: '0 auto 0 auto',
+  },
+  iframe: {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    bottom: '0',
+    right: '0',
+    width: '75%',
+    height: '75%',
+    margin: '0 auto 0 auto',
+  },
+};
+
 function BookPage() {
   const [book, setBook] = useState();
   const [author, setAuthor] = useState();
   const [illustrator, setIllustrator] = useState();
+  const [collections, setCollections] = useState([]);
+
+  //  Grab collections
+  const getCollections = () => {
+    base('Collection').select({ view: 'Grid view' }).all() // Gets + returns all records
+      .then((records) => { // Takes in returned records + calls setPosts to store in posts arr
+        setCollections(records);
+      });
+  };
+  useEffect(getCollections, []);
 
   // Instead of using props, we pull bookId from URL
   const params = useParams();
@@ -56,6 +90,11 @@ function BookPage() {
   let readAloudURL;
   let bookshopURL;
   let educatorURLs;
+  let identityTags;
+  let ageMin;
+  let ageMax;
+  let gradeMin;
+  let gradeMax;
 
   if (book) {
     title = (book.get('title')) ? book.get('title') : title;
@@ -63,7 +102,12 @@ function BookPage() {
     image = (book.get('image')) ? book.get('image') : [{ url: Logo }];
     readAloudURL = (book.get('read_aloud_link')) ? book.get('read_aloud_link') : null;
     bookshopURL = (book.get('bookshop_link')) ? book.get('bookshop_link') : null;
-    educatorURLs = (book.get('educator_guide_links')) ? book.get('educator_guide_links').split('\n') : [];
+    educatorURLs = (book.get('educator_guide_link')) ? book.get('educator_guide_link').split('\n') : [];
+    identityTags = (book.get('identity_tags')) ? book.get('identity_tags') : [];
+    ageMin = (book.get('age_min')) ? book.get('age_min') : -1;
+    ageMax = (book.get('age_max')) ? book.get('age_max') : -1;
+    gradeMin = (book.get('grade_min')) ? book.get('grade_min') : -1;
+    gradeMax = (book.get('grade_max')) ? book.get('grade_max') : -1;
   }
 
   if (author) {
@@ -76,12 +120,26 @@ function BookPage() {
     illustratorID = (illustrator.get('id')) ? illustrator.get('id') : illustratorID;
   }
 
-  const imageURL = image[0].url;
-
+  
   const synopsisProps = {
-    title, authorName, authorID, illustratorName, illustratorID, desc, imageURL,
+    title,
+    authorName,
+    authorID,
+    illustratorName,
+    illustratorID,
+    desc,
+    imageURL,
+    bookshopURL,
+    readAloudURL,
+    educatorURLs,
+    identityTags,
+    ageMin,
+    ageMax,
+    gradeMin,
+    gradeMax,
   };
 
+  const imageURL = image[0].url;
   const isValidUrl = (string) => {
     /* Validate url given by TCK. URL must start with http or https protocol. */
     let url;
@@ -93,14 +151,8 @@ function BookPage() {
     }
     return url.protocol === 'http:' || url.protocol === 'https:';
   };
+  educatorURLs.filter((string) => isValidUrl(string));
 
-  const educatorLinks = educatorURLs.map((url) => (
-    <ListItem button component="a" href={url} key={url} alignItems="center">
-      <ListItemText sx={{ textAlign: 'center' }} primary={url} />
-    </ListItem>
-  ));
-
-  educatorLinks.filter((string) => isValidUrl(string));
 
   if (title === 'Untitled Book') {
     return (<h1>{'Sorry, we couldn\'t retrieve this book from our library 😔'}</h1>);
@@ -108,35 +160,30 @@ function BookPage() {
 
   return (
     <Paper variant="outlined">
+      <CollectionsCarousel
+        elementArray={collections}
+        slidesAtATime={6}
+        prevArrow={LeftArrow}
+        nextArrow={RightArrow}
+        widthPercent={100}
+        spaceBetweenEntries={16}
+        swiperHeight={120}
+        cardImageHeightPercent={80}
+        cardImageWidthPercent={80}
+      />
       <BookSynopsis {...synopsisProps} />
-      {(readAloudURL) ? (
-        <div className="video-responsive">
+      {(readAloudURL) && (
+        <Box sx={styles.iframeContainer}>
           <iframe
-            width="853"
-            height="480"
+            style={styles.iframe}
             src={`https://www.youtube.com/embed/${readAloudURL.split('watch?v=')[1]}`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             title="Embedded youtube"
           />
-        </div>
-      ) : <div />}
-      {(bookshopURL) ? (
-        <LinkUI href={bookshopURL} rel="noreferrer" target="_blank">
-          Buy
-          {' '}
-          {title}
-          !
-        </LinkUI>
-      ) : <div />}
-      <h4>Educator Guides</h4>
-      {(educatorURLs)
-        ? (
-          <List>
-            {educatorLinks}
-          </List>
-        ) : <div />}
+        </Box>
+      ) }
     </Paper>
   );
 }
