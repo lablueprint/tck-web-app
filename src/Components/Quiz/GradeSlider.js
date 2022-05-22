b09787929c7bc44eda9602dd50f0dca64752e8a8
Avnish Sengupta <avnishsengupta1@g.ucla.edu>
2022-05-22 01:28:55 -0700

    set default slider to prek to 12

--- src/Components/Quiz/GradeSlider.js
+++ src/Components/Quiz/GradeSlider.js
@@ -64,10 +64,10 @@ function valueLabelFormat(value) {
 const minDistance = 0;
 
 export default function GradeSlider({ parentCallback, parentCallbackButton }) {
-  const [value1, setValue1] = React.useState([4, 7]);
+  const [value1, setValue1] = React.useState([-1, 12]);
   const [isDisabledSlider, setIsDisabledSlider] = React.useState();
-  const [min, setMin] = React.useState(4);
-  const [max, setMax] = React.useState(7);
+  const [min, setMin] = React.useState(-1);
+  const [max, setMax] = React.useState(12);
 
   const handleChange1 = (event, newValue, activeThumb) => {
     setIsDisabledSlider(false);
