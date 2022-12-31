dc52e36adc4b2d53e503049be4f400e8ef53d12f
Diya Baliga <diyabaliga@Diyas-MacBook-Air.local>
2022-12-31 04:56:41 -0800

    updated header styling and added drawer

--- src/Components/Navigation/Header.js
+++ src/Components/Navigation/Header.js
@@ -1,13 +1,17 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React, { useState, useEffect } from 'react';
 import './Header.css';
 import {
-  NavLink,
+  NavLink, useLocation, useNavigate,
 } from 'react-router-dom';
 import {
-  IconButton, MenuItem, Drawer, Box, CssBaseline,
+  IconButton, MenuItem, Drawer, Box, Tabs, Tab, AppBar,
 } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import CloseIcon from '@mui/icons-material/Close';
 import { v4 as uuidv4 } from 'uuid';
+import NorthEastIcon from '@mui/icons-material/NorthEast';
 import Logo from '../../Assets/Images/TCK SVG Logo.svg';
 
 const options = [
@@ -43,105 +47,187 @@ function useWindowSize() {
   return windowSize;
 }
 
+const styles = {
+  drawerNavLink: {
+    fontFamily: 'DM Sans',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: '1.3rem',
+    color: '#292929',
+    paddingLeft: '2rem',
+  },
+  header: {
+    background: 'white',
+    boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)',
+    minHeight: '50px',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0 2%',
+    zIndex: (theme) => theme.zIndex.drawer + 1,
+  },
+};
+
+const HeaderTabs = styled(Tabs)({
+  height: 'inherit',
+  '& .MuiTabs-indicator': {
+    backgroundColor: '#393EBA',
+    height: '4px',
+  },
+});
+
+const HeaderTab = styled((props) => <Tab disableRipple {...props} />)(
+  {
+    textTransform: 'none',
+    fontFamily: 'Work Sans',
+    fontWeight: 'normal',
+    fontSize: '1rem',
+    color: '##3F3F3F',
+    height: 'inherit',
+    margin: '8px auto',
+    '&.Mui-selected': {
+      fontWeight: 800,
+      color: '#393EBA',
+    },
+  },
+);
+
 function Header() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [active, setActive] = useState(false);
-  const open = Boolean(anchorEl);
-
-  const handleClick = () => {
-    setAnchorEl(!open);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleDrawerToggle = () => {
-    setAnchorEl(!open);
+    setMobileOpen((last) => !last);
   };
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
 
   const drawer = (
     <Box
       role="presentation"
       onClick={handleDrawerToggle}
       sx={{
-        width: { xs: '100%', sm: 250 },
+        width: { xs: '100vw', sm: 300 },
+        marginTop: { xs: '8rem', sm: '6rem' },
       }}
     >
       {options.map((option) => (
         <NavLink
           to={option.nav}
-          isActive={(match) => {
-            if (match) {
-              setActive(true);
-            }
-          }}
           key={uuidv4()}
+          onClick={handleDrawerToggle}
+          className={({ isActive }) => (isActive ? 'active' : 'nav-style')}
         >
-          <MenuItem key={option.name} selected={active} onClick={handleClose}>
+          <MenuItem
+            sx={styles.drawerNavLink}
+            key={option.name}
+            onClick={handleDrawerToggle}
+          >
             {option.name}
           </MenuItem>
         </NavLink>
       ))}
+      <MenuItem sx={{ marginTop: '2rem' }} key="tck-url">
+        <a className="tck-url" href="https://www.theconsciouskid.org">
+          theconsciouskid.org
+        </a>
+        <NorthEastIcon sx={{ fontSize: '1rem' }} />
+      </MenuItem>
     </Box>
   );
 
   const size = useWindowSize();
   return (
     <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <nav className="header">
-        <div className="header-container">
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? 'nav-link-active logo-img-container' : 'nav-link logo-img-container')}
-          >
-            <img src={Logo} className="logo" alt="The Conscious Kid logo" />
-          </NavLink>
-          { size.width > 910 ? (
-            <ul className="nav-menu">
-              {options.map((option) => (
-                <li>
-                  <NavLink
-                    to={option.nav}
-                    className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')}
-                    key={uuidv4()}
-                  >
-                    {option.name}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <IconButton
-              id="long-button"
-              onClick={handleClick}
-            >
-              <MenuRoundedIcon
-                className="menu-icon"
-                fontSize="large"
+      <AppBar position="fixed" sx={styles.header}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? 'nav-link-active logo-img-container' : 'nav-link logo-img-container')}
+        >
+          <img src={Logo} className="logo" alt="The Conscious Kid logo" />
+        </NavLink>
+        { size.width > 910 ? (
+          <HeaderTabs value={location.pathname}>
+            {options.map((option) => (
+              <HeaderTab
+                key={uuidv4()}
+                value={option.nav}
+                onClick={() => navigate(option.nav)}
+                label={option.name}
               />
-            </IconButton>
-          )}
-        </div>
-      </nav>
-      <div>
+            ))}
+          </HeaderTabs>
+        ) : (
+          <IconButton
+            id="long-button"
+            onClick={handleDrawerToggle}
+          >
+            {mobileOpen
+              ? <CloseIcon className="menu-icon" fontSize="large" />
+              : <MenuRoundedIcon className="menu-icon" fontSize="large" />}
+          </IconButton>
+        )}
+      </AppBar>
+      <Box component="nav">
         <Drawer
-          id="long-menu"
+          variant="temporary"
           anchor="right"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          variant="persistent"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
           ModalProps={{
             keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { sm: 'block', md: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box' },
           }}
         >
           {drawer}
         </Drawer>
-      </div>
+      </Box>
     </Box>
   );
 }
 
 export default Header;
+
+// <Tabs
+// color="primary"
+// value={alignment}
+// onChange={handleChange}
+// sx={styles.tabGroup}
+// >
+// <Tab
+//   value="Filter"
+//   label="Filter"
+//   icon={<Tune />}
+//   iconPosition="start"
+//   sx={styles.tab}
+// />
+// <Tab
+//   value="Search"
+//   label="Search"
+//   icon={<Search />}
+//   iconPosition="start"
+//   sx={styles.tab}
+// />
+// </Tabs>
+
+// const styles = {
+//   tab: {
+//     textTransform: 'none',
+//     font: 'DM Sans',
+//     fontWeight: 'bold',
+//     fontSize: '15px',
+//     paddingBottom: '0px',
+//   },
+//   tabGroup: {
+//     margin: '1vh auto 4vh 4vw',
+//   },
+// };
