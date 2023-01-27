1b0eee3f047dff13abf047e29aa46063b6052415
Diya Baliga <diyabaliga@Diyas-Air.lan>
2023-01-26 21:05:23 -0800

    pulled url out

--- src/airtable.js
+++ src/airtable.js
@@ -4,9 +4,13 @@ const Airtable = require('airtable');
 const airtableConfig = {
   apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
   baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
+  endpointUrl: process.env.REACT_APP_AIRTABLE_ENDPOINT_URL,
 };
 
-const base = new Airtable({ apiKey: airtableConfig.apiKey, endpointUrl: 'https://proxy.sequin.io/api.airtable.com' })
+const base = new Airtable({
+  apiKey: airtableConfig.apiKey,
+  endpointUrl: airtableConfig.endpointUrl,
+})
   .base(airtableConfig.baseKey);
 
 export default base;
